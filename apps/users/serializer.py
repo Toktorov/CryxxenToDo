56bd593a381780fa664622c4b31531ebc05325b8
Toktorov <ktoktorov2005@gmail.com>
2022-12-25 20:09:32 +0600

    phone number validate

--- apps/users/serializer.py
+++ apps/users/serializer.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from apps.users.models import User
 from apps.todo.serializers import ToDoSerializer
+from django.core.validators import RegexValidator
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -35,10 +36,19 @@ class UserRegisterSerializer(serializers.ModelSerializer):
     def validate(self, attrs):
         if attrs['password'] != attrs['password2']:
             raise serializers.ValidationError({"password": "Пароли отличаются"})
+        if '+996' not in attrs['phone_number']:
+            raise serializers.ValidationError({"phone_number": "Напишите номер с +996"})
         return attrs
 
     def create(self, validated_data):
-        user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
+        user = User.objects.create(
+            username=validated_data['username'],
+            email = self.initial_data.get("email"),
+            phone_number = self.initial_data.get("phone_number"),
+            age = self.initial_data.get("age"),
+        )
+        user.set_password(validated_data['password'])
+        user.save()
         return user
 
 class UserDetailSerializer(serializers.ModelSerializer):
